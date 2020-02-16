b354381f8cc397dac6b16d68bdbc3c7a4db20605
Abhay123-art <60810551+Abhay123-art@users.noreply.github.com>
2020-02-16 19:22:16 +0530

    Add files via upload

---
+++ abhaySINGH.py
@@ -0,0 +1,51 @@
+a = 5
+b = 15
+print(a+b)
+
+a = 5
+b = 15
+c = 7
+print(b-a+c)
+
+a = 30
+b = 25
+c =  a*b
+print(c)
+
+a = 500
+b = 19
+print(a/b)
+
+a = 30
+b = 40
+c = 6
+print(a*c/b)
+
+a = 5
+b = 6
+print(a**b)
+
+a = "abhay "
+b = "singh "
+c = 'rajput'
+print(a+b+c)
+
+print("*" *5)
+print("#" *4)
+
+x = 5
+print(x**5)
+
+Email='''
+hi
+i'm abhay singh rajput
+I'm from madhya pradesh
+'''
+print(Email)
+
+a = 40
+b = 3
+print(a//b)
+
+a = ("python programming")
+print(len(a))
