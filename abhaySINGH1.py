39882f3319915107a4f0e78a272ef41bb60bdf35
Abhay123-art <60810551+Abhay123-art@users.noreply.github.com>
2020-02-17 17:13:20 +0530

    Add files via upload

---
+++ abhaySINGH1.py
@@ -0,0 +1,55 @@
+a = 10
+b = 3
+print(a/b)
+#True Division
+
+a = 10
+b = 3
+print(a//b)
+#Floor Division
+
+a = 10.7
+print(round(a))
+print(round(10.3))
+
+a = 15
+print(float(a))
+
+a = f"{2+2}+{10%3}"
+print(a)
+
+name = "abhay singh rajput"
+print(name.find("singh"))
+print(name.find("rajput"))
+print(name.find("y"))
+
+name = "abhay singh rajput"
+print(name.replace("a","r"))
+
+name =  "abhay singh rajput"
+print(name[2])
+print(name[6])
+print(name[-3])
+print(name[2:-4])
+print(len(name))
+
+a = 5
+print(a**2**2)
+
+name = input("whats your name? ")
+print("hi "+name)
+
+name = input("whats your name? ")
+hobby = input("whats your hobby? ")
+print(name+ " likes to "+hobby)
+
+birth_year = input("birth year- ")
+age =  2020 -int(birth_year)
+print(age)
+
+str = "india's pm narendra modi"
+print(str.upper())
+print(str.lower())
+
+
+
