aef3739e9bc8505b30c7c67af3f51390760f00d7
Abhay123-art <60810551+Abhay123-art@users.noreply.github.com>
2020-02-18 16:26:09 +0530

    Add files via upload

---
+++ abhaySINGH_18_FEB.py
@@ -0,0 +1,65 @@
+import math
+z = 4.7
+print(math.ceil(z))
+#print the ceiling value of z
+
+print(math.floor(z))
+#print the floor value of z
+
+x = 5
+print(math.factorial(x))
+#to find factorial of x
+
+print(math.gcd(225,550))
+#to find greatest common divisor
+
+x = 10.6
+print(math.modf(x))
+#to find out fractional and integar parts of x seperately
+
+x = 34
+y = 4
+print(math.remainder(x,y))
+#to find out remainder of x/y
+
+x = 5
+y = 5
+print(math.pow(x,y))
+#to find out x to the power y
+
+x = 625
+print(math.sqrt(x))
+#to find out square root of x
+
+print(math.degrees(15))
+#convert angle to radian to degrees
+
+print(math.radians(200))
+#convert degrees to radian
+
+x = 5
+print(math.gamma(x))
+#print gamma function at x
+
+#mathematical constant
+
+print(math.pi)
+#to find of value of pi
+
+print(math.e)
+#to find the value of e
+
+print(math.tau)
+#to find out the value of tau
+
+x = 5
+print(math.tau*x)
+print(5*math.pi/2)
+
+print(math.sin(0))
+print(math.cos(0))
+print(math.tan(45))
+#to find out exact value of sin,cos and tan
+
+
+
